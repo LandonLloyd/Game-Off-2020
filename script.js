b74c2446ec22d85c492ef05e67c54f545de3fc52
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-11-12 14:00:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,82 @@
 let gameOver = document.getElementById('gameOver');
+
 let player = document.getElementById('player');
+
 let bullet = document.getElementById('bullet');
+
 let eyes = document.getElementById('eyes');
-let colors = ['green', 'blue', 'red', 'orange', 'black'];
+
+let start = document.getElementById('startGame');
+
+let colors = ['green', 'blue', 'red', 'orange', 'pink', 'cyan'];
+
 let i = Math.floor(Math.random() * colors.length);
+
 let number = document.getElementById('number');
+
 let score = 0;
+
 let random = Math.floor(Math.random() * 140);
 
-document.addEventListener('keydown', event => {
-  if(event.key === "ArrowUp" || event.keyCode === 32){
-    player.style.height = 50 + "px";
-    player.style.top = 150 + "px";
-    
-    if(player.classList != "animation"){
-      player.classList.add("animation");
-    }
-    player.classList.add("animation");
-    setTimeout(function(){
-      player.classList.remove("animation");
-    }, 500);
+window.onload = bullet.classList.remove('fly');
+window.onload = bullet.style.display = 'none';
+
+  function startGame(){
+  bullet.classList.add('fly');
+  bullet.style.display = 'initial';
+  gameOver.innerText = '';
+  score = 0;
   }
   
-  if(event.key === "ArrowDown"){
-    player.classList != "animation";
-    player.style.height = 25 + "px";
-    player.style.top = 175 + "px";
-  }
-  });
+  document.addEventListener('keydown', event => {
+    if(event.key === "ArrowUp" || event.keyCode === 32){
+      player.style.height = 50 + "px";
+      player.style.top = 150 + "px";
+      
+      if(player.classList != "animation"){
+        player.classList.add("animation");
+      }
+      player.classList.add("animation");
+      setTimeout(function(){
+        player.classList.remove("animation");
+      }, 500);
+    }
+    
+    if(event.key === "ArrowDown"){
+      player.classList != "animation";
+      player.style.height = 25 + "px";
+      player.style.top = 175 + "px";
+    }
+    });
+
+  /*document.addEventListener('keydown', event => {
+    if(event.key === "ArrowUp" || event.keyCode === 32){
+      player.style.height = 50 + "px";
+      player.style.top = 150 + "px";
+      
+      if(player.classList != "animation"){
+        player.classList.add("animation");
+      }
+      player.classList.add("animation");
+      setTimeout(function(){
+        player.classList.remove("animation");
+      }, 500);
+    }
+    
+    if(event.key === "ArrowDown"){
+      player.classList != "animation";
+      player.style.height = 25 + "px";
+      player.style.top = 175 + "px";
+    }
+    });*/
+
 
 let checkDead = setInterval(function(){
+  
    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+   
    let bulletLeft = parseInt(window.getComputedStyle(bullet).getPropertyValue("left"));
+   
    if(bulletLeft < 20 && bulletLeft > 0 && playerTop >= 130){
      bullet.style.animation = "none";
      bullet.style.display = "none";
@@ -39,12 +84,11 @@ let checkDead = setInterval(function(){
    }else if(bullet.style.display != "none"){
      score++;
      number.innerText = Math.floor(score / 100);
+     
    }
    
 }, 10);
-if(i === 'black'){
-  eyes.style.backgroundColor === 'red';
-}
+
 
 bullet.style.backgroundColor = colors[i];
 player.style.backgroundColor = colors[i];
