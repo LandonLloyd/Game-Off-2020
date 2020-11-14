caeec08d4ce6ef3c5f98350d12c9bcf453501c72
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-11-13 22:37:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let gameOver = document.getElementById('gameOver')
+let gameOver = document.getElementById('gameOver');
 
-let player = document.getElementById('player')
+let player = document.getElementById('player');
 
-let bullet = document.getElementById('bullet')
+let bullet = document.getElementById('bullet');
 
-let eyes = document.getElementById('eyes')
+let eyes = document.getElementById('eyes');
 
-let start = document.getElementById('startGame')
+let start = document.getElementById('startGame');
 
-let board = document.getElementById('leaderboard')
+let board = document.getElementById('leaderboard');
+
 
 let colors = [
     'green',
@@ -22,62 +23,73 @@ let colors = [
     'olive',
     'coral',
     'slategray',
-    'amazon',
-    'amber',
-    'acid green'
-]
+    'aqua',
+    'aquamarine',
+    'chocolate',
+    'coral',
+    'crimson',
+    'DarkMagenta',
+    'indigo',
+    'HotPink',
+    'magenta',
+    'maroon',
+    'navy',
+    'salmon',
+    'DodgerBlue',
+    'FireBrick'
+];
 
-let i = Math.floor(Math.random() * colors.length)
+let i = Math.floor(Math.random() * colors.length);
 
-let number = document.getElementById('number')
+let number = document.getElementById('number');
 
-let score = 0
+let score = 0;
 
-let random = Math.floor(Math.random() * 140)
+let random = Math.floor(Math.random() * 140);
 
 function startGame() {
-    document.location.reload()
+    document.location.reload();
 }
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowUp' || event.keyCode === 32) {
-        player.style.height = 50 + 'px'
-        player.style.top = 150 + 'px'
-
+        player.style.height = 50 + 'px';
+        player.style.top = 150 + 'px';
+        
         if (player.classList != 'animation') {
-            player.classList.add('animation')
+            player.classList.add('animation');
         }
-        player.classList.add('animation')
+        player.classList.add('animation');
         setTimeout(function () {
-            player.classList.remove('animation')
-        }, 500)
+            player.classList.remove('animation');
+        }, 500);
     }
 
     if (event.key === 'ArrowDown') {
-        player.classList != 'animation'
-        player.style.height = 25 + 'px'
-        player.style.top = 175 + 'px'
+        player.classList != 'animation';
+        player.style.height = 25 + 'px';
+        player.style.top = 175 + 'px';
     }
-})
+});
 
 let checkDead = setInterval(function () {
     let playerTop = parseInt(
         window.getComputedStyle(player).getPropertyValue('top')
-    )
+    );
 
     let bulletLeft = parseInt(
         window.getComputedStyle(bullet).getPropertyValue('left')
-    )
+    );
 
     if (bulletLeft < 20 && bulletLeft > 0 && playerTop >= 130) {
-        bullet.style.animation = 'none'
-        bullet.style.display = 'none'
-        player.style.animation = 'none'
+        bullet.style.animation = 'none';
+        bullet.style.display = 'none';
+        player.style.animation = 'none';
         gameOver.innerText =
             'You lose! :( You Jumped over ' +
             Math.floor(score / 100) +
-            ' rocks!'
-        if (localStorage.getItem('Score') < score) {
+            ' rocks!';
+        if (score > Number(localStorage.getItem("Score"))) {
             localStorage.setItem(
                 'Name',
                 prompt(
@@ -85,20 +97,22 @@ let checkDead = setInterval(function () {
                         Math.floor(score / 100) +
                         '. Please enter your name so you can be named the All Time Best!'
                 )
-            )
-            localStorage.setItem('Score', Math.floor(score / 100))
+              
+            );
+            localStorage.setItem("Score", Math.floor(score / 100));
         }
     } else if (bullet.style.display != 'none') {
-        score++
-        number.innerText = Math.floor(score / 100)
+        score++;
+        number.innerText = Math.floor(score / 100);
     }
-}, 10)
+}, 10);
 
 board.innerText =
     'All Time Best: ' +
     localStorage.getItem('Name') +
     ' With ' +
-    localStorage.getItem('Score')
-bullet.style.backgroundColor = colors[i]
-player.style.backgroundColor = colors[i + 1 || 0]
-gameOver.style.color = colors[i]
+    localStorage.getItem('Score');
+bullet.style.backgroundColor = colors[i];
+player.style.backgroundColor = colors[i + 1 || 0];
+gameOver.style.color = colors[i];
+console.log(localStorage.getItem("Score"));
